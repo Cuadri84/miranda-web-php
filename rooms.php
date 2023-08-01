1cee9a693e64b4c3d400d8c06218d56ff9ed25c9
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-08-02 01:04:44 +0200

    Rooms data from database

--- rooms.php
+++ rooms.php
@@ -1,16 +1,5 @@
 <?php include('./bladeConfig.php');
 echo $blade->run("rooms");
 
-include("database.php");
-$sql = "SELECT * FROM rooms";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " ;
-    }
-  } else {
-    echo "0 results";
-  }
-  $conn->close();
+
 ?>
